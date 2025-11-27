08acb44b36455acc4c419dfdc8bae489bf88cd08
Timofey Kulyassov <xocastillian@gmail.com>
2025-11-27 23:16:24 +0500

    lang transaction

--- src/screens/Statistics/StatisticsScreen.tsx
+++ src/screens/Statistics/StatisticsScreen.tsx
@@ -1,8 +1,7 @@
 const StatisticsScreen = () => {
 	return (
 		<div className='flex min-h-full flex-col items-center justify-center gap-2 p-3 pb-24 text-center text-sm '>
-			<div className='text-lg font-medium '>Статистика</div>
-			<p>Здесь позже появится аналитика расходов и доходов.</p>
+			<div className='text-lg font-medium '>Soon</div>
 		</div>
 	)
 }
